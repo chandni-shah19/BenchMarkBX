e83a0e1573e1a91144a5c5f0c8d56426ba2e17a2
Anthony Anjorin <tonyanjorin@googlemail.com>
2016-10-21 21:03:30 +0200

    Completed Medini QVT impl.

--- examples/familiestopersons/testsuite/BenchmarxFamiliesToPersons/src/org/benchmarx/examples/familiestopersons/implementations/qvtr/MediniQVTR.java
+++ examples/familiestopersons/testsuite/BenchmarxFamiliesToPersons/src/org/benchmarx/examples/familiestopersons/implementations/qvtr/MediniQVTR.java
@@ -1,45 +1,116 @@
 package org.benchmarx.examples.familiestopersons.implementations.qvtr;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.function.Consumer;
 
 import org.benchmarx.BXTool;
 import org.benchmarx.Configurator;
 import org.benchmarx.examples.familiestopersons.testsuite.Decisions;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 
+import Families.FamiliesFactory;
+import Families.FamiliesPackage;
 import Families.FamilyRegister;
 import Persons.PersonRegister;
+import Persons.PersonsPackage;
+import de.ikv.emf.qvt.EMFQvtProcessorImpl;
+import de.ikv.medini.qvt.QVTProcessorConsts;
+import uk.ac.kent.cs.kmf.util.ILog;
+import uk.ac.kent.cs.kmf.util.OutputStreamLog;
 
 public class MediniQVTR implements BXTool<FamilyRegister, PersonRegister, Decisions> {
+	private ILog logger;
+	private EMFQvtProcessorImpl processorImpl;
+	private ResourceSet resourceSet;
+	private Resource source;
+	private Resource target;
+	private String basePath;
+	private String transformation;
+	private FileReader qvtRuleSet;
+	private static final String fwdDir = "perDB";
+	private static final String bwdDir = "famDB";
+	
+	public MediniQVTR() {
+		logger = new OutputStreamLog(System.out);
+		processorImpl = new EMFQvtProcessorImpl(this.logger);
+		processorImpl.setProperty(QVTProcessorConsts.PROP_DEBUG, "true");
+		basePath = "./src/org/benchmarx/examples/familiestopersons/implementations/qvtr/base/";
+		
+		// Tell the QVT engine, which transformation to execute
+		transformation = "families2persons";
 
+		// Tell the QVT engine a directory to work in - e.g. to store the trace (meta)models
+		File tracesFile = new File(basePath + "/traces/trace.trafo");
+		tracesFile.delete();
+	}
+	
 	@Override
 	public void initiateSynchronisationDialogue() {
-		// TODO Auto-generated method stub
+		// Initialise resource set of models
+		this.resourceSet = new ResourceSetImpl();
+		this.resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put(
+		    Resource.Factory.Registry.DEFAULT_EXTENSION, new XMIResourceFactoryImpl());
+
+		// Create resources for models
+		source = resourceSet.createResource(URI.createURI("source.xmi"));
+		target = resourceSet.createResource(URI.createURI("target.xmi"));
+		
+		// Collect all necessary packages from the metamodel(s)
+		Collection<EPackage> metaPackages = new ArrayList<EPackage>();
+		this.collectMetaModels(metaPackages);
+
+		// Make these packages known to the QVT engine
+		init(metaPackages);
+		
+		// Collect the models, which should participate in the transformation.
+		// You can provide a list of models for each direction.
+		// The models must be added in the same order as defined in your transformation!
+		Collection<Collection<Resource>> modelResources = new ArrayList<Collection<Resource>>();
+		Collection<Resource> firstSetOfModels = new ArrayList<Resource>();
+		Collection<Resource> secondSetOfModels = new ArrayList<Resource>();
+		modelResources.add(firstSetOfModels);
+		modelResources.add(secondSetOfModels);
+		firstSetOfModels.add(source);
+		secondSetOfModels.add(target);
+		
+		URI directory = URI.createFileURI(basePath + "/traces");
+		this.preExecution(modelResources, directory);
+		
+		source.getContents().add(FamiliesFactory.eINSTANCE.createFamilyRegister());
+		launch(fwdDir);
 	}
 
 	@Override
 	public void performAndPropagateTargetEdit(Consumer<PersonRegister> edit) {
-		// TODO Auto-generated method stub
-		
+		edit.accept(getTargetModel());
+		launch(bwdDir);
 	}
 
 	@Override
 	public void performAndPropagateSourceEdit(Consumer<FamilyRegister> edit) {
-		// TODO:  Do sth with the source edit?
-		
-		RunTrafo_FWD simpleQVT = new RunTrafo_FWD(System.out);
-		simpleQVT.launch("./src/org/benchmarx/examples/familiestopersons/implementations/qvtr/base/", "target.xmi", "source.xmi");
+		edit.accept(getSourceModel());
+		launch(fwdDir);
 	}
 
 	@Override
 	public FamilyRegister getSourceModel() {
-		// TODO Auto-generated method stub
-		return null;
+		return (FamilyRegister) source.getContents().get(0);
 	}
 
 	@Override
 	public PersonRegister getTargetModel() {
-		// TODO Auto-generated method stub
-		return null;
+		return (PersonRegister) target.getContents().get(0);
 	}
 
 	@Override
@@ -47,4 +118,71 @@ public class MediniQVTR implements BXTool<FamilyRegister, PersonRegister, Decisi
 		// Medini QVT does not support interaction
 	}
 
+	/**
+	 * Provide the information about the metamodels, which are involved in the transformation
+	 * 
+	 * @param ePackages
+	 *            the metamodel packages
+	 */
+	public void init(Collection<EPackage> ePackages) {
+		Iterator<EPackage> iterator = ePackages.iterator();
+		while (iterator.hasNext()) {
+			this.processorImpl.addMetaModel(iterator.next());
+		}
+	}
+
+	/**
+	 * Before transforming, the engine has to know the model to perform the transformation on, as
+	 * well as a directory for the traces to store.
+	 * 
+	 * @param modelResources
+	 *            models for the execution - take care of the right order!
+	 * @param workingDirectory
+	 *            working directory of the QVT engine
+	 */
+	public void preExecution(Collection<?> modelResources, URI workingDirectory) {
+		this.processorImpl.setWorkingLocation(workingDirectory);
+		this.processorImpl.setModels(modelResources);
+	}
+
+	/**
+	 * Transform a QVT script in a specific direction.
+	 * 
+	 * @param qvtRuleSet
+	 *            the QVT transformation
+	 * @param transformation
+	 *            name of the transformation
+	 * @param direction
+	 *            name of the target - must conform to your QVT transformation definition
+	 * @throws Throwable
+	 */
+	public void transform(Reader qvtRuleSet, String transformation, String direction) throws Throwable {
+		processorImpl.evaluateQVT(qvtRuleSet, transformation, true, direction, new Object[0], null, this.logger);
+	}
+
+	public void launch(String direction) {
+		// Load the QVT relations
+		try {
+			qvtRuleSet = new FileReader(basePath + "/families2persons.qvt");
+		} catch (FileNotFoundException fileNotFoundException) {
+			fileNotFoundException.printStackTrace();
+			return;
+		}
+		try {
+			this.transform(qvtRuleSet, transformation, direction);
+		} catch (Throwable throwable) {
+			throwable.printStackTrace();
+		}
+	}
+
+	/**
+	 * Add all metamodel packages of models/qvt script
+	 * 
+	 * @param metaPackages
+	 * @return
+	 */
+	protected void collectMetaModels(Collection<EPackage> metaPackages) {
+		metaPackages.add(PersonsPackage.eINSTANCE);
+		metaPackages.add(FamiliesPackage.eINSTANCE);
+	}
 }
